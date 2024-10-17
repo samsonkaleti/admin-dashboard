f60c4b6ffbd6c71f9520a2c48a100c0980547041
samsonkaleti <kaletishyam@gmail.com>
2024-10-17 23:35:21 +0530

    added logo and improved styling in nav bar and added sech bar on nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Sidebar } from "./dashboard/components/sidebar";
-import { UserNav } from "./dashboard/components/user-nav";
+import { UserNav } from "./dashboard/components/user-nav"; 
+import { Search } from "./dashboard/components/search";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,10 +22,11 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="flex h-screen overflow-hidden">
           <Sidebar />
-          <div className="flex-1 flex flex-col overflow-hidden">
-            <header className="sticky top-0 z-50 border-b bg-background">
+          <div className="flex-1 flex flex-col overflow-hidden ">
+            <header className="sticky top-0 z-50 border-b bg-background px-4">
               <div className="container flex h-16 items-center justify-between py-4">
-                <h1 className="text-2xl font-bold">College Admin</h1>
+                <h1 className="text-2xl font-bold">College Admin</h1> 
+                <Search />
                 <UserNav />
               </div>
             </header>
